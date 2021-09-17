9b3e06cdb8e7c9c10429e6118d691136da52768b
little-pinecone <marta.szymek.mail@gmail.com>
2021-09-17 08:47:36 +0200

    feat: initial commit

---
+++ src/test/java/in/keepgrowing/springbootcontrolleradvice/SpringBootControllerAdviceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.keepgrowing.springbootcontrolleradvice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootControllerAdviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
