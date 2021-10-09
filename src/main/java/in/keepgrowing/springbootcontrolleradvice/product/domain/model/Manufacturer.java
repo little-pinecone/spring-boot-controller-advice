e06477cd20ece31ba3ac0f7590844f10127742f9
little-pinecone <marta.szymek.mail@gmail.com>
2021-10-09 19:35:29 +0200

    perf: add validation rule for Manufacturer::contactEmail

--- src/main/java/in/keepgrowing/springbootcontrolleradvice/product/domain/model/Manufacturer.java
+++ src/main/java/in/keepgrowing/springbootcontrolleradvice/product/domain/model/Manufacturer.java
@@ -4,7 +4,7 @@ import lombok.Builder;
 import lombok.Data;
 
 import javax.validation.constraints.Email;
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 
 @Data
 @Builder
@@ -13,6 +13,6 @@ public class Manufacturer {
     private String name;
 
     @Email
-    @NotNull
+    @NotBlank
     private String contactEmail;
 }
